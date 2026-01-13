Nexters/Chac-Android

[#4] chore: `:data:photo` 모듈 생성

data/photo/build.gradle.kts
@@ -1,0 +1,10 @@
+import com.chakchak.convention.extensions.setNamespace
+
+plugins {
+    alias(libs.plugins.chakchak.android.library)
+    alias(libs.plugins.chakchak.android.room)
+}
+
+android {
+    setNamespace("data.photo")
+}

data/photo/src/main/java/com/chakchak/data/photo/MyClass.kt
@@ -1,0 +1,3 @@
+package com.chakchak.data.photo
+
+class MyClass

Nexters/ChakChak-Android

data/photo/build.gradle.kts
@@ -1,0 +1,10 @@
+import com.chac.convention.extensions.setNamespace
+
+plugins {
+    alias(libs.plugins.chac.android.library)
+    alias(libs.plugins.chac.android.room)
+}
+
+android {
+    setNamespace("data.photo")
+}

data/photo/src/main/java/com/chac/data/photo/MyClass.kt
@@ -1,0 +1,3 @@
+package com.chac.data.photo
+
+class MyClass