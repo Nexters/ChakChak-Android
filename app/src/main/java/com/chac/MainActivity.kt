Nexters/Chac-Android

[#4] refactor: `:core:designsystem` 모듈 생성 및 테마 관련 파일 이동
- 작업 사유: 멀티모듈 구조에서 공통으로 사용될 디자인 시스템을 분리하여 관리하기 위함
- 작업 내용
    - `:core:designsystem` 모듈 생성
    - `Color`, `Type`, `Theme` 파일을 `:app` 모듈에서 `:core:designsystem` 모듈로 이동
    - `:app` 모듈이 `:core:designsystem` 모듈을 구현하도록 의존성 추가

app/src/main/java/com/chakchak/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.chakchak.ui.theme.ChakChakTheme
+import com.chakchak.core.designsystem.ui.theme.ChakChakTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint

core/designsystem/build.gradle.kts
@@ -1,0 +1,10 @@
+import com.chakchak.convention.extensions.setNamespace
+
+plugins {
+    alias(libs.plugins.chakchak.android.library)
+    alias(libs.plugins.chakchak.android.compose)
+}
+
+android {
+    setNamespace("core.designsystem")
+}

core/designsystem/src/main/java/com/chakchak/core/designsystem/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.chakchak.ui.theme
+package com.chakchak.core.designsystem.ui.theme
 
 import androidx.compose.ui.graphics.Color
 

core/designsystem/src/main/java/com/chakchak/core/designsystem/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.chakchak.ui.theme
+package com.chakchak.core.designsystem.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme

core/designsystem/src/main/java/com/chakchak/core/designsystem/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.chakchak.ui.theme
+package com.chakchak.core.designsystem.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle

Nexters/ChakChak-Android

app/src/main/java/com/chac/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.chac.ui.theme.ChacTheme
+import com.chac.core.designsystem.ui.theme.ChacTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint

core/designsystem/build.gradle.kts
@@ -1,0 +1,10 @@
+import com.chac.convention.extensions.setNamespace
+
+plugins {
+    alias(libs.plugins.chac.android.library)
+    alias(libs.plugins.chac.android.compose)
+}
+
+android {
+    setNamespace("core.designsystem")
+}

core/designsystem/src/main/java/com/chac/core/designsystem/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.chac.ui.theme
+package com.chac.core.designsystem.ui.theme
 
 import androidx.compose.ui.graphics.Color
 

core/designsystem/src/main/java/com/chac/core/designsystem/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.chac.ui.theme
+package com.chac.core.designsystem.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme

core/designsystem/src/main/java/com/chac/core/designsystem/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.chac.ui.theme
+package com.chac.core.designsystem.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle