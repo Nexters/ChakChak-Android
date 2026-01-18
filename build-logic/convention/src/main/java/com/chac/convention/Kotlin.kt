fc1b618d85a6b9d32d8359f7dc3afe0dc2a2b7b2
SBKIM <s_been@kakao.com>
2026-01-19 04:32:19 +0900

    [#12] chore: 코틀린 컨벤션(convention/Kotlin.kt)에 코루틴 의존성 추가

--- build-logic/convention/src/main/java/com/chac/convention/Kotlin.kt
+++ build-logic/convention/src/main/java/com/chac/convention/Kotlin.kt
@@ -1,20 +1,23 @@
 package com.chac.convention
 
+import com.chac.convention.extensions.libs
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.tasks.compile.JavaCompile
-import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import kotlin.collections.addAll
 
 internal fun Project.configureKotlin() {
     tasks.withType<JavaCompile>().configureEach {
         sourceCompatibility = JavaVersion.VERSION_21.majorVersion
         targetCompatibility = JavaVersion.VERSION_21.majorVersion
+    }
+
+    dependencies {
+        val libs = project.extensions.libs
+        "implementation"(libs.findLibrary("kotlinx.coroutines.core").get())
     }
 
     tasks.withType<KotlinCompile>().configureEach {
